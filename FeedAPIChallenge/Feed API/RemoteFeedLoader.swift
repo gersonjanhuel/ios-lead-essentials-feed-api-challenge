04bc691efbc1ce7d2ed9522c4b5743a6f40086eb
Gerson Janhuel <gersonjanhuel@gmail.com>
2021-08-28 15:26:15 +0700

    make sure not deliver result if instance has beed deallocated. prevent memory leak.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			// to prevent memory leak
+			guard self != nil else { return }
+
 			switch result {
 			case let .success((data, response)):
 				guard response.statusCode == 200 else {
